505f4c3c89b55842a81e3ddf0e54a1109c034f0e
chiara1911 <chiaracesari89@gmail.com>
2023-10-10 14:03:26 +0200

    add const in js and input in html

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+// dati necessari per calcolo
+
+// km = 0.21;
+
+// sconto 20% minorenni;
+// sconto 40% over 65;
+//let = quanti anni hai;
+// let= quanti km vuoi fare?
+
+const tariffaKm = 0.21;
+
+const discount18 = 0.2;
+const discount65 = 0.4;
+
+console.log(tariffaKm);
+console.log(discount18);
+console.log(discount65);
+
