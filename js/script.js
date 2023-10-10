4eee080515b049549d4b2ffce4619bfd2ee83f5c
chiara1911 <chiaracesari89@gmail.com>
2023-10-10 15:47:19 +0200

    ticket

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@
 //let = quanti anni hai;
 // let= quanti km vuoi fare?
 
+// calcoli
 const tariffaKm = 0.21;
 
 const discount18 = 0.2;
@@ -16,10 +17,9 @@ const discount65 = 0.4;
 console.log(tariffaKm);
 console.log(discount18);
 console.log(discount65);
-let distanza = 10;
-let age = 81;
-let price = (tariffaKm * distanza);
 
+let age = 81;
+// let price = (tariffaKm * distanza);
 
 
 if (age < 18){
@@ -29,4 +29,16 @@ if (age < 18){
     price = price - (price * discount65);
 }
 
+const button = document.querySelector('btn');
+
+button.addEventListener('click', function(){
+
+    let nome = document.getElementById('nome');
+    let distanza = document.getElementById('distanza');
+    let age = document.getElementById('age');
+
+    const price = (tariffaKm * distanza);
+
+   
+});
 console.log(price);
